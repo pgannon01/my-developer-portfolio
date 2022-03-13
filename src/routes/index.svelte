ec25676ecea107be2fa418ee16c31be1c7f7b8c5
Patrick Gannon <gannon.patrick01@gmail.com>
2022-03-12 22:43:43 -0500

    Add first component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,7 @@
 <script context="module">
-import { client } from '$lib/graphql-client';
-
-    import { gql } from 'graphql-request'
+    import ProjectCard from '$lib/components/project-card.svelte';
     import { client } from '$lib/graphql-client';
+    import { gql } from 'graphql-request'
 
     export const load = async() => {
 
@@ -45,5 +44,10 @@ import { client } from '$lib/graphql-client';
     export let projects;
 </script>
 
-<pre>{JSON.stringify(projects, null, 2)}</pre> 
-<!-- Above for testing purposes -->
+<h1>Recent Projects I've Worked On!</h1>
+
+<div>
+    {#each projects as { name, slug, description, image }}
+        <ProjectCard {name} {description} url={image[0].url} {slug} />
+    {/each}
+</div>
