c2359c87b9bebb07294e31334b5efda2a8ad6493
Patrick Gannon <gannon.patrick01@gmail.com>
2022-03-12 22:48:08 -0500

    Add Tailwind CSS and daisyui

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						maxWidth: null,
+					},
+				},
+			},
+		},
+	},
+
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui'),
+	]
+};
+
+module.exports = config;
