6848965385169a4e3b3063ddcdd4281be16e87aa
Patrick Gannon <gannon.patrick01@gmail.com>
2022-03-16 21:54:41 -0400

    Add footer component

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { svg_element } from "svelte/internal";
+    import GithubIcon from "./github-icon.svelte";
+    import LinkedinIcon from "./linkedin-icon.svelte";
+
+</script>
+<footer class="p-10 footer bg-primary text-primary-content footer-center">
+    <div class="grid grid-flow-col gap-4">
+        <a class="link link-hover" href="/projects">Portfolio</a> 
+        <a class="link link-hover" href="/posts">Blog</a> 
+        <a class="link link-hover" href="/about">About</a> 
+    </div>
+    <div>
+        <div class="grid grid-flow-col gap-4">
+            <a href="https://github.com">
+                <GithubIcon />
+            </a> 
+            <a href="https://linkedin.com">
+                <LinkedinIcon />
+            </a> 
+        </div>
+    </div>
+    <div>
+        <p>
+            Copyright &copy; {`${new Date().getFullYear()}`} - All rights reserved 
+            by Me
+        </p>
+    </div>
+</footer>
